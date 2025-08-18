40cbf1a19628f5e2b608e22cfb81f77cedb65462
MaxFell <67173616+MaxFell310@users.noreply.github.com>
2025-08-18 03:02:16 +0300

    Add files via upload

---
+++ lesson-2/script/script.js
@@ -0,0 +1,14 @@
+// const userName = prompt('Введіть Ім\'я', 'Шановний')
+
+// document.write(`
+//     <h1>Доброго дня ${userName}, у нас на проєкті!</h1>
+//     `)
+
+
+// const num1 = parseFloat(prompt('Перше число', '0'))
+// const num2 = parseFloat(prompt('Друге число', '0'))
+// const sum = num1 + num2
+
+// document.write(`
+//     <h1> Сума ваших чисел ${sum}!</h1>
+//     `)
